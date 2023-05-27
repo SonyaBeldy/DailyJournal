ab74ef261e4c1c1a17edbe2f428e0d9dcb82faa1
Sonya <sofya.beldy@mail.ru>
2023-05-27 17:28:36 +1000

    Task entity

---
+++ src/main/java/ru/sonyabeldy/dailyjournal/controllers/ToDoController.java
@@ -0,0 +1,11 @@
+package ru.sonyabeldy.dailyjournal.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todo")
+public class ToDoController {
+
+
+}
--- src/main/java/ru/sonyabeldy/dailyjournal/models/Task.java
+++ src/main/java/ru/sonyabeldy/dailyjournal/models/Task.java
@@ -2,6 +2,8 @@ package ru.sonyabeldy.dailyjournal.models;
 
 import jakarta.persistence.*;
 
+import java.sql.Timestamp;
+
 @Entity
 @Table(name = "Task")
 public class Task {
@@ -11,6 +13,77 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
 
+    @Column(name = "title")
+    String title;
 
+    @Column(name = "description")
+    String description;
 
+    @Column(name = "creation_date")
+    Timestamp creationDate;
+
+    @Column(name = "completion_date")
+    Timestamp completionDate;
+
+    public Task() {
+    }
+
+    public Task(int id, String title, String description, Timestamp creationDate, Timestamp completionDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.creationDate = creationDate;
+        this.completionDate = completionDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Timestamp getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(Timestamp completionDate) {
+        this.completionDate = completionDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", creationDate=" + creationDate +
+                ", completionDate=" + completionDate +
+                '}';
+    }
 }
